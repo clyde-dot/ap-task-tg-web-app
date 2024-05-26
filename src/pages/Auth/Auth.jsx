3dacb84e76af80d12319ce46b4ec7c8117c39d4f
ishhit <satybaldiev.ishhit@gmail.com>
2024-05-26 16:55:07 +0500

    init new pages

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./Auth.css";
 import useTelegram from "../../hooks/useTelegram";
+
 const Auth = () => {
     const { tg } = useTelegram();
     const [userPassword, setUserPassword] = useState("");
+    const [userRole, setUserRole] = useState("OPERATOR");
     tg.MainButton.setParams({ text: "Войти" });
 
+    const onSendData = useCallback(() => {
+        const data = { userPassword, userRole };
+        tg.sendData(JSON.stringify(data));
+    }, [tg, userPassword, userRole]);
+
+    useEffect(() => {
+        tg.onEvent("mainButtonClicked", onSendData);
+        return () => {
+            tg.offEvent("mainButtonClicked", onSendData);
+        };
+    }, [tg, onSendData]);
     const onChangePassword = (e) => {
         setUserPassword(e.target.value);
         if (userPassword.length >= 8) {
@@ -14,16 +27,34 @@ const Auth = () => {
             tg.MainButton.hide();
         }
     };
+    const onChangeRole = (e) => {
+        setUserRole(e.target.value);
+    };
     return (
         <div className="form-auth">
             <div className="form-title">Авторизация пользователя в системе</div>
             <div className="form-input">
-                <input
-                    onChange={onChangePassword}
-                    value={userPassword}
-                    type="password"
-                    placeholder="Введите секретный ключ:"
-                />
+                <div>
+                    <span>Ключ: </span>
+                    <input
+                        onChange={onChangePassword}
+                        value={userPassword}
+                        type="password"
+                        placeholder="Введите секретный ключ..."
+                    />
+                </div>
+                <div>
+                    <span>Роль :</span>
+                    <select value={userRole} onChange={onChangeRole}>
+                        <option value="OPERATOR">Оператор</option>
+                        <option value="OPERATOR-ADMIN">Главный Оператор</option>
+                        <option value="PROGRAMMER">Программист</option>
+                        <option value="PROGRAMMER-ADMIN">
+                            Главный программист
+                        </option>
+                        <option value="ADMIN">Админ</option>
+                    </select>
+                </div>
             </div>
         </div>
     );
